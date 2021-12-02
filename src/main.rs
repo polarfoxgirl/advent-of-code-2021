a6de26511dfbe14c6ffc15fcdaed45586a7f20d2
polarfoxgirl <helga.nikonova@gmail.com>
2021-12-01 22:51:00 -0500

    Add day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!!");
+    let depths = read_input(String::from("src\\day01\\input.txt"));
+    println!("Input count: {}", depths.len());
+
+    let mut count = 0;
+    for i in 0..(depths.len() - 1) {
+        if depths[i] < depths[i+1] {
+            count = count + 1;
+        }        
+    }
+
+    println!("Result: {}", count)
+}
+
+fn read_input(filename: String) -> Vec<i32> {
+    println!("Reading file {}", filename);
+    let text = fs::read_to_string(filename).expect("Failed to read input");
+
+    text.split("\r\n").map(|x| x.parse().unwrap()).collect()
 }
