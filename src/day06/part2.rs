360cef022d35e6d43cb2c00b1b9de411c4ee08d1
polarfoxgirl <helga.nikonova@gmail.com>
2021-12-09 21:04:53 -0500

    Solve day 6 part 2

--- src/day06/part2.rs
+++ src/day06/part2.rs
@@ -1,3 +1,45 @@
+use std::fs;
+
 #[allow(dead_code)]
 pub fn solve() {
+    let fish_states = read_input(&String::from("src\\day06\\inputs\\input.txt"));
+    println!("Input count: {}", fish_states.len());
+
+    let mut state_counts = vec![0i64; 9];
+    count_init_states(&fish_states, &mut state_counts);
+
+    for _ in 0..256 {
+        apply_day_step(&mut state_counts);
+    }
+
+    let result : i64 = state_counts.iter().sum();
+    println!("Result: {}", result)
+}
+
+fn read_input(filename: &String) -> Vec<usize> {
+    println!("Reading file {}", filename);
+    let text = fs::read_to_string(filename).expect("Failed to read input");
+
+    text.split(",").map(|x| x.parse().unwrap()).collect()
+}
+
+fn count_init_states(fish_states: &Vec<usize>, state_count: &mut Vec<i64>) {
+    for state in fish_states {
+        if *state > 8 {
+            panic!("Invalid initial fish state {}", state);
+        }
+
+        state_count[*state] += 1;
+    }
+}
+
+fn apply_day_step(state_counts: &mut Vec<i64>) {
+    // Remove all fish with 0 state so every state effectively shifts
+    let reproduction_count = state_counts.remove(0);
+
+    // Append newly created fish in the end, effectively to the 8 state
+    state_counts.push(reproduction_count);
+
+    // Reset counter for all newly reproduced fish to 6
+    state_counts[6] += reproduction_count;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ extern crate regex;
 mod day06;
 
 fn main() {
-    day06::part1::solve()
+    day06::part2::solve()
 }
